e8e44d2b6153da70db117b449be4c52b9caf07e7
MrCalico <CalicoCat@Hotmail.com>
2018-03-10 20:22:27 -0800

    generated tachometer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { TachometerComponent } from './tachometer/tachometer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TachometerComponent
   ],
   imports: [
     BrowserModule
